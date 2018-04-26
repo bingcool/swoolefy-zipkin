d11e47ef671af535a979298d73225874470c322d
huangzengbing@banggood.cn <huangzengbing@banggood.cn>
2018-04-26 13:04:35 +0800

    完善调用链

---
+++ tests/test.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "../vendor/autoload.php";
+
+use zipkin\ZipkinHander;
+
+$zipkin = ZipkinHander::getInstance('http://192.168.99.102', '9411', false);
+
+$zipkin->setEndpoint('swoolefy service1', '192.168.99.102', 80);
+
+$zipkin->setTracer('/Test/test');
+
+//这里开始创建一个span 
+$begainSpanInfo = $zipkin->begainSpan();
+list($requireStartTime, $spanId) = $begainSpanInfo;
+
+$url = 'https://jsonplaceholder.typicode.com/posts/1';
+	$context = stream_context_create([
+	    'http' => [
+	        'method' => 'GET',
+	        'header' =>
+	            'X-B3-TraceId: ' . $zipkin->getTraceId() . "\r\n" .
+	            'X-B3-SpanId: ' . ((string) $spanId) . "\r\n" .
+	            'X-B3-ParentSpanId: ' . $zipkin->getTraceSpanId() . "\r\n" .
+	            'X-B3-Sampled: ' . $zipkin->isSampled() . "\r\n"
+	    ]
+	]);
+$request = file_get_contents($url, false, $context);
+
+// 这里结束一个span
+$zipkin->afterSpan($begainSpanInfo, ['jsonplaceholder API', '104.31.87.157', '80'], 'posts/1');
+
+
+
+$zipkin->trace(true);
